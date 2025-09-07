614f7a14e237cda17ac61aed101a0f9ffcef5754
Vini Moraes <wvinim@msn.com>
2025-09-07 18:58:51 -0300

    test: remove test files

--- backend/src/recipes/entities/recipes.entity/recipes.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RecipesEntity } from './recipes.entity';
-
-describe('RecipessEntity', () => {
-  it('should be defined', () => {
-    expect(new RecipesEntity()).toBeDefined();
-  });
-});
