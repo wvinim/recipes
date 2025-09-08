0671ec70b5899feacca8db23652926acf9daf057
Vini Moraes <wvinim@msn.com>
2025-09-08 18:33:35 -0300

    feat: axios 401 to login

--- frontend/src/axios-config.js
+++ frontend/src/axios-config.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
+import router from './router';
 
 const api = axios.create({
   baseURL: 'http://localhost:3000',
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      router.push('/login');
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
