5904ceafcb6a82837d3cd4da474c326f97de1342
Vini Moraes <wvinim@msn.com>
2025-09-08 11:41:06 -0300

    feat: login

---
+++ frontend/src/components/Auth/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <h2>Login de Usuário</h2>
+    <div v-if="error" class="error">{{ error }}</div>
+    <div>
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email" required />
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" required />
+    </div>
+    <button type="submit" :disabled="loading">
+      {{ loading ? 'Entrando...' : 'Entrar' }}
+    </button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import api from '../../axios-config';
+
+const email = ref('');
+const password = ref('');
+const error = ref(null);
+const loading = ref(false);
+
+const submitForm = async () => {
+  loading.value = true;
+  error.value = null;
+
+  try {
+    const response = await api.post('http://localhost:3000/auth/login', {
+      email: email.value,
+      password: password.value,
+    });
+    router.push('/recipes');
+  } catch (err) {
+    if (err.response && err.response.data.message) {
+      error.value = err.response.data.message;
+    } else {
+      error.value = 'Ocorreu um erro. Tente novamente mais tarde.';
+    }
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
